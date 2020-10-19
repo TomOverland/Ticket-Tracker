0abf48a6907e2a95debde483c32306335c35afd9
Tom Overland <tomoverland03@gmail.com>
2020-10-19 18:57:40 -0500

    created index route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,32 +1,27 @@
-// This is example code to be used as a reference
-
-
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
+var db = require("../models");
+var sequelize = require("sequelize");
 var path = require("path");
+var fs = require("fs");
+var util = require("util");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
+  // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/html/user/index.html"));
   });
 
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
   });
-
 };
+
