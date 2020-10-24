e57f8b2af08b8da0f981928081505ea8187a05bf
Tom Overland <tomoverland03@gmail.com>
2020-10-24 12:54:42 -0500

    added completed PUT request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all ticketss
+  // Get all tickets
   app.get("/api/tickets", function(req, res) {
     db.Ticket.findAll({}).then(function(dbTickets) {
       res.json(dbTickets);
@@ -9,15 +9,18 @@ module.exports = function(app) {
   });
 
   // Create a new ticket
-  app.post("/api/tickets", function(req, res) {
+  app.post("/submit/api/tickets", function(req, res) {
+    console.log('req.body', req.body);
+
     db.Ticket.create(req.body).then(function(dbTicket) {
+      
       res.json(dbTicket);
     });
   });
 
   // trying to get view single ticket to post notes
   app.put("/ticket/api/tickets", function(req, res, next) {
-    
+    console.log("note entry");
     const reqNotes = req.body;
 
     db.Ticket.update(
@@ -26,6 +29,21 @@ module.exports = function(app) {
       res.json(updatedTicket);
     }).catch(next);
   })
+
+
+  app.put("/ticket/api/updated", function(req, res, next) {
+    console.log("apiRequest");
+    const reqCompleted = req.body;
+    console.log(reqCompleted);
+
+    db.Ticket.update(
+      {completed: reqCompleted.completed},
+      { where: { id: reqCompleted.id }}).then(function(updateComplete) {
+        res.json(updateComplete);
+      }).catch(next);
+    
+  });
+
 
 // Update ticket status to complete
   app.put("/api/tickets/:id", function(req, res) {
